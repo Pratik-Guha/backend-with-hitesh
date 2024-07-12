0f68cd4df01570f41bbb13a791276bbad751f84a
Pratik-Guha <sguha4966@gmail.com>
2024-07-13 00:29:58 +0530

    allset

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-import React from 'react'
+// require('dotenv').config({path:'./.env'})
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+// import { DB_NAME } from './constants';
+import express from 'express';
+import connectDB from './db/index.js';
 
-function index() {
-  return (
-    <div>index</div>
-  )
-}
+dotenv.config({path:'./.env'})
 
-export default index
+
+
+connectDB()
+
+
+/*const app=express();
+
+(async()=>{
+   try {
+      await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+      app.on("error",(error)=>{
+        console.log("error",error)
+      })
+
+      app.listen(process.env.PORT,()=>{
+         console.log(`App is liseting on port ${process.env.PORT}`)
+      })
+   } catch (error) {
+      console.error("ERROR ",error)
+      throw error
+   }
+})()*/
