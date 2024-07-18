9b4ac72f0b1a4b8bdb8278796df9e9a83250409e
Pratik-Guha <sguha4966@gmail.com>
2024-07-18 12:39:38 +0530

    commit

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,21 +1,24 @@
-import { v2 as cloudinary } from "cloudinary"
-import fs from "fs"
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+// const cloudinary = require('cloudinary'); 
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-})
-
+  secure: true,
+});
 const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return nulll
     //upload the file on cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto"
-    }) // file has been uploAded on cloudinary
-    // console.log("file has been uploaded on cloudinary",response.url);
-    fs.unlinkSync(localFilePath) //remove the locally saved file
+    }) 
+    // file has been uploAded on cloudinary
+    console.log("file has been uploaded on cloudinary",response.url);
+    // fs.unlinkSync(localFilePath) //remove the locally saved file
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath) //remove the locally saved file as the upload operation  got failed
