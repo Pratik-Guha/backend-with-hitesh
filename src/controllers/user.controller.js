582631ef235a63f478dad05aa8cdae4834af9621
Pratik-Guha <sguha4966@gmail.com>
2024-07-25 01:32:26 +0530

    new routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -438,5 +438,5 @@ const getWatchHistory= asyncHandler(async (req,res)=>{
 })
 export {
   registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser
-  , updateAccountDetails, updateUserAvatar, updateUserCoverImage,getWatchHistory
+  , updateAccountDetails, updateUserAvatar, updateUserCoverImage,getWatchHistory,getUserChannelProfile
 }
